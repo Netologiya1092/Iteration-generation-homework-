53ac2c74ea829ca12a9fe2e651f4d557f464637f
Netologiya1092 <dzahaman51@gmail.com>
2026-02-19 21:14:48 +0500

    Add flat_generator to flatten nested lists

---
+++ 4case.py
@@ -0,0 +1,10 @@
+import types
+
+def flat_generator(list_of_list):
+    for item in list_of_list:
+        if isinstance(item, list):
+            # Рекурсивно "вытягиваем" элементы из вложенного списка
+            yield from flat_generator(item)
+        else:
+            yield item
+
